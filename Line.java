b8a4fb4ffefffa07748ab237d999dac98ed1aab0
Aysha-shamli <ayshashamli1999@gmail.com>
2023-12-04 13:52:04 +0530

    added program to check equality of two line

--- Line.java
+++ Line.java
@@ -1,3 +1,4 @@
+
 public class Line {
     private int x1, y1, x2, y2;
     public Line(int x1, int y1, int x2, int y2) {
@@ -13,8 +14,15 @@ public class Line {
 
         display();
 
-        Line line = new Line(15, 15, 20, 20);
-        double lineLength = Math.sqrt(Math.pow((line.x2 - line.x1), 2) + Math.pow((line.y2 - line.y1), 2));
-        System.out.println("Length of the line between Point 1 and Point 2 is: " + lineLength);
+        Line line1 = new Line(15, 15, 20, 20);
+        Line line2 = new Line(5, 5, 10, 10);
+        double line1Length = Math.sqrt(Math.pow((line1.x2 - line1.x1), 2) + Math.pow((line1.y2 - line1.y1), 2));
+        double line2Length = Math.sqrt(Math.pow((line2.x2 - line2.x1), 2) + Math.pow((line2.y2 - line2.y1), 2));
+        if (line1Length == line2Length){
+            System.out.println("The lines are equal");
+        }
+        else{
+            System.out.println("The lines are not equal");
+        }
     }
 }
