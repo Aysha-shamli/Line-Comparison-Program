0ebab46b84d6c02a9487fd7401258e37487f0107
Aysha-shamli <ayshashamli1999@gmail.com>
2023-12-04 13:41:01 +0530

    added display program into master

---
+++ Line.java
@@ -0,0 +1,9 @@
+public class Line {
+    public static void display(){
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+    public static void main(String[] args) {
+        display();
+
+    }
+}
